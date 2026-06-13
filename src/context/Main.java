89577ddb712298a61e422124f024ebff4e6b9cef
Juan Manuel Eraso <juaneraso2003@hotmail.com>
2026-06-13 10:25:40 -0500

    Primer ejercicio APOII reorganizado

---
+++ src/context/Main.java
@@ -0,0 +1,18 @@
+package context;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Atributos
+		int d = 0;
+        int v = 4;
+        int t = 8;
+        
+        // Formula
+        d = (v * t);
+        
+        //Resultado
+        System.out.println("Distancia: " + d);
+	}
+
+}
